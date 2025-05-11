59c1a41574cb16586c8fc9efa2303f549f20cb5c
Kavindu Dilshan <kavindu4543@gmail.com>
2025-05-11 17:09:27 +0530

    Authentication Service complted with Security configurations

---
+++ auth-service/src/main/java/com/kavindu/authservice/service/UserService.java
@@ -0,0 +1,21 @@
+package com.kavindu.authservice.service;
+
+import com.kavindu.authservice.model.User;
+import com.kavindu.authservice.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+}
